4816867fc1ad931c72b35c0009f18bb6c9f375c7
Alina <alina.andrevna3993@gmail.com>
2023-09-26 14:25:13 +0300

    added quiz

---
+++ pages/quiz.js
@@ -0,0 +1,13 @@
+import Page from "components/common/Page";
+import Quiz from "components/pages/Quiz";
+
+
+function QuizPage({  }) {
+  return (
+    <Page title="Quiz">
+      <Quiz />
+    </Page>
+  );
+}
+
+export default QuizPage;
