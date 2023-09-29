bbd5cae11b106535fe07c940797faa5cc49b8674
Alexey Glushchenko <alex.glushchenko@saltpepper.ru>
2023-09-29 17:13:21 +0300

    Added client and server

--- client/pages/quiz.js
+++ client/pages/quiz.js
@@ -1,8 +1,8 @@
 import Page from "components/common/Page";
+import SocketManager from "components/common/SocketManager/SocketManager";
 import Quiz from "components/pages/Quiz";
 
-
-function QuizPage({  }) {
+function QuizPage({}) {
   return (
     <Page title="Quiz">
       <Quiz />
