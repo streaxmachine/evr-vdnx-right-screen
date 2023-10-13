2fbbb988567481fd5c9d9d31c9be7f21a62f9e21
Alina <alina.andrevna3993@gmail.com>
2023-10-13 17:01:29 +0300

    margin update

--- components/common/OnWay/OnWay.jsx
+++ components/common/OnWay/OnWay.jsx
@@ -47,7 +47,8 @@ const OnWay = ({
 
   React.useEffect(() => {
     if (progress === 320) {
-      setState(5);
+      // setState(5);
+      null
     }
   }, [progress]);
 
@@ -72,28 +73,45 @@ const OnWay = ({
   const calculateManualYPosition = (progressPercent) => {
     const intervals = [
       { start: 0, end: 9, value: "0rem" },
-      { start: 9, end: 12, value: "12rem" },
-      { start: 12, end: 16, value: "-4rem" },
-      { start: 16, end: 20, value: "12rem" },
-      { start: 20, end: 24, value: "-4rem" },
-      { start: 24, end: 28, value: "12rem" },
-      { start: 28, end: 32, value: "-4rem" },
-      { start: 32, end: 36, value: "12rem" },
-      { start: 36, end: 40, value: "-4rem" },
-      { start: 40, end: 44, value: "12rem" },
-      { start: 44, end: 48, value: "-4rem" },
-      { start: 48, end: 52, value: "12rem" },
-      { start: 52, end: 56, value: "-4rem" },
-      { start: 56, end: 60, value: "12rem" },
-      { start: 60, end: 64, value: "-4rem" },
-      { start: 64, end: 68, value: "12rem" },
-      { start: 68, end: 72, value: "-4rem" },
-      { start: 72, end: 76, value: "12rem" },
-      { start: 76, end: 80, value: "-4rem" },
-      { start: 80, end: 82, value: "12rem" },
+      { start: 9, end: 12, value: "14rem" },
+      { start: 12, end: 14, value: "-18rem" },
+      { start: 14, end: 16, value: "0rem" },
+      { start: 16, end: 18, value: "14rem" },
+      { start: 18, end: 20, value: "0rem" },
+      { start: 20, end: 22, value: "-18rem" },
+      { start: 22, end: 24, value: "0rem" },
+      { start: 24, end: 26, value: "14rem" },
+      { start: 26, end: 28, value: "0rem" },
+      { start: 28, end: 30, value: "-18rem" },
+      { start: 30, end: 32, value: "0rem" },
+      { start: 32, end: 34, value: "14rem" },
+      { start: 34, end: 36, value: "0rem" },
+      { start: 36, end: 38, value: "-18rem" },
+      { start: 38, end: 40, value: "0rem" },
+      { start: 40, end: 42, value: "14rem" },
+      { start: 42, end: 44, value: "0rem" },
+      { start: 44, end: 48, value: "-18rem" },
+      { start: 42, end: 44, value: "0rem" },
+      { start: 48, end: 50, value: "14rem" },
+      { start: 50, end: 52, value: "0rem" },
+      { start: 52, end: 54, value: "-18rem" },
+      { start: 54, end: 56, value: "0rem" },
+      { start: 56, end: 58, value: "14rem" },
+      { start: 58, end: 60, value: "0rem" },
+      { start: 60, end: 62, value: "-18rem" },
+      { start: 62, end: 64, value: "0rem" },
+      { start: 64, end: 66, value: "14rem" },
+      { start: 66, end: 68, value: "0rem" },
+      { start: 68, end: 70, value: "-18rem" },
+      { start: 72, end: 74, value: "0rem" },
+      { start: 72, end: 74, value: "14rem" },
+      { start: 74, end: 76, value: "0rem" },
+      { start: 76, end: 78, value: "-18rem" },
+      { start: 78, end: 80, value: "0rem" },
+      { start: 80, end: 82, value: "14rem" },
       { start: 82, end: 100, value: "0rem" },
     ];
-  
+
     for (const interval of intervals) {
       if (progressPercent >= interval.start && progressPercent < interval.end) {
         return interval.value;
