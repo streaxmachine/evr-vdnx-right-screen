0727bda365f80d7e5e7ca0a91041eeef6a5845df
Alina <alina.andrevna3993@gmail.com>
2023-10-14 11:34:00 +0300

    Quiz First Page added

---
+++ components/common/QuizFirstPage/QuizFirstPage.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+import s from "./QuizFirstPage.module.scss";
+
+const Guide = ({ setState }) => {
+  return (
+    <>
+      <main className={s.page}>
+        <div className={s.clouds}></div>
+        <div className={s.textblock}>
+          <h2 className={s.title}>
+            Приветствуем вас
+            <p>в игровой зоне!</p>
+          </h2>
+          <span className={s.text}>
+          Знакомьтесь с проектами Тверской области в игровом формате 
+          <p> и получите возможность выиграть приз!</p>
+          </span>
+          <button
+            onClick={() => {
+              setState(2);
+            }}
+            className={s.button}
+          >
+            Выбрать игру
+          </button>
+        </div>
+        <section className={s.imgContainer}>
+          <img
+            className={s.img0}
+            src="/images/background/q_zig.png"
+            alt="Зигзаг"
+          />
+          <div className={s.aiChat} />
+          <div className={s.speechBubble}>
+            AI comment AI comment AI comment AI comment AI comment{" "}
+          </div>
+        </section>
+      </main>
+    </>
+  );
+};
+export default Guide;
--- pages/quiz.js
+++ pages/quiz.js
@@ -1,11 +1,13 @@
 import Page from "components/common/Page";
+import QuizCards from "components/common/QuizCards";
+import QuizFirstPage from "components/common/QuizFirstPage";
 import SocketManager from "components/common/SocketManager/SocketManager";
 import Quiz from "components/pages/Quiz";
 
 function QuizPage({}) {
   return (
     <Page title="Quiz">
-      <Quiz />
+      <QuizFirstPage />
     </Page>
   );
 }
