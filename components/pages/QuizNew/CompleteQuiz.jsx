06c068382e48270a1651ebd5d8a151acfa41cc40
Alexey Glushchenko <alex.glushchenko@saltpepper.ru>
2023-10-26 17:49:14 +0300

    Added screen logic

--- components/pages/QuizNew/CompleteQuiz.jsx
+++ components/pages/QuizNew/CompleteQuiz.jsx
@@ -4,6 +4,25 @@ import useStore from "hooks/useStore";
 
 import s from "./TouchPanel/TouchPanel.module.scss";
 
+const phrases = [
+  "Вы дошли до финала! Спасибо за участие в игре. Ваш процент правильных ответов:  .  Получите подарок у сотрудника стенда!",
+  "Вот и финиш! Вы достойно справились. Ваш процент правильных ответов: ___. За участие вам полагается приз! Вы можете получить его у сотредника стенда!",
+  "Поздравляю! Вы прекрасно преодолели все испытания. Ваш результат: ___. Обязательно заберите награду за свои старания у сотрудника стенда!",
+  "Поздравляю! Вы прекрасно преодолели все испытания. Ваш результат: ___. Обязательно заберите награду за свои старания у сотрудника стенда!",
+];
+
+const makePhraseWithProcent = (scenario, percent) => {
+  if (scenario === 1) {
+    return `Вы дошли до финала! Спасибо за участие в игре. Ваш процент правильных ответов:  ${percent}%.  Получите подарок у сотрудника стенда!`;
+  } else if (scenario === 2) {
+    return `Вот и финиш! Вы достойно справились. Ваш процент правильных ответов: ${percent}%. За участие вам полагается приз! Вы можете получить его у сотредника стенда!`;
+  } else if (scenario === 3) {
+    return `Поздравляю! Вы прекрасно преодолели все испытания. Ваш результат: ${percent}%. Обязательно заберите награду за свои старания у сотрудника стенда!`;
+  } else if (scenario === 4) {
+    return `Игра завершена! Вы точно не коренной тверичанин? Вы дали ${percent}% правильных ответов. За такой грандиозный результат вам однозначно полагается приз! Возьмите награду у сотрудника стенда!`;
+  }
+};
+
 const CompleteQuiz = ({
   score,
   socket,
@@ -29,36 +48,40 @@ const CompleteQuiz = ({
     );
   }, []);
 
+  const phraseNumber = React.useMemo(() => {
+    if (percent < 50) {
+      return 1;
+    } else if (percent < 65) {
+      return 2;
+    } else if (percent < 85) {
+      return 3;
+    } else if (percent < 100) {
+      return 4;
+    }
+  }, [percent]);
+
   React.useEffect(() => {
-    console.log(time);
     time === "000:000"
-      ? setScenario({ type: "quiz", place: "timesUp" })
-      : setScenario({ type: "quiz", place: "gameOver" })
-  }, []);
+      ? setScenario({ type: "quiz", place: "timeIsOut" })
+      : setScenario({ type: "quiz", place: "results" });
+  }, [time]);
 
   return (
     <div className={s.completeRoot}>
       <div className={s.completeContainer}>
         <div>
-          {percent > 50 ? (
-            <img src="/images/completeSuccess/win.svg" alt="" />
-          ) : (
+          {time === "000:000" ? (
             <img src="/images/completeSuccess/lose.svg" alt="" />
+          ) : (
+            <img src="/images/completeSuccess/win.svg" alt="" />
           )}
         </div>
         <p className={s.completeTitle}>Игра завершена</p>
         <div className={s.completeText}>
-          {percent > 50 ? (
-            <p>
-              Вы точно не коренной тверичанин? Вы дали {percent}% правильных
-              ответов. За такой грандиозный результат вам однозначно полагается
-              приз! Возьмите награду у промоутера стенда!
-            </p>
+          {time === "000:000" ? (
+            `К сожалению, время вышло! Ваш результат ${percent}% правильных ответов. Попробуйте еще раз!`
           ) : (
-            <p>
-              К сожалению, время вышло! Ваш результат {percent}% правильных
-              ответов. Попробуйте еще раз!
-            </p>
+            <>{makePhraseWithProcent(phraseNumber, percent)}</>
           )}
         </div>
         <div className={s.completeResults}>
