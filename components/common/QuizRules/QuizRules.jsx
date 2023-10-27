06c9475480db3a93b2ce7605f677c28fdca17501
Alina <alina.andrevna3993@gmail.com>
2023-10-27 11:54:51 +0300

    quizRules page changed

--- components/common/QuizRules/QuizRules.jsx
+++ components/common/QuizRules/QuizRules.jsx
@@ -31,25 +31,33 @@ const QuizRules = ({ setGlobalState, socket }) => {
               Вам предстоит проверить свои знания о Тверской области. Вас ждет
               12 вопросов, поделенных на 4 категории. Игра продолжается до тех
               пор, пока не окончится время. Постарайтесь дать как можно больше
-              правильных ответов. После завершения игры вы сможете получить приз
-              у промоутера стенда! Удачи!
+              правильных ответов. <p> После завершения игры вы сможете получить приз
+              у промоутера стенда! Удачи!</p>
             </span>
           </div>
-          <section className={s.cardsWrapper}>
+          {/* <section className={s.cardsWrapper}>
             {cards.map((card, id) => (
               <Card card={card} key={id} id={id} />
             ))}
-          </section>
+          </section> */}
 
-          <button
-            className={s.playBtn}
-            onClick={() => {
-              setGlobalState("touchPanel");
-              console.log("clicked");
-            }}
-          >
-            Все понятно! Я в игре
-          </button>
+          <section className={s.bottomContainer}>
+            <button
+              className={s.playBtn}
+              onClick={() => {
+                setGlobalState("touchPanel");
+                console.log("clicked");
+              }}
+            >
+              Все понятно! Я в игре
+            </button>
+            <div className={s.imgContainer}>
+              <img src="/images/QuizRules/01.png" className={s.img01} alt=""></img>
+              <img src="/images/QuizRules/02.png" className={s.img02} alt=""></img>
+              <img src="/images/QuizRules/03.png" className={s.img03} alt=""></img>
+              <img src="/images/QuizRules/04.png" className={s.img04} alt=""></img>
+            </div>
+          </section>
         </div>
 
         <div className={s.clouds}></div>
@@ -59,13 +67,13 @@ const QuizRules = ({ setGlobalState, socket }) => {
 };
 export default QuizRules;
 
-function Card({ card }) {
-  return (
-    <div className={`${s.card}`}>
-      <div className={s.cardTextBlock}>
-        <h4 className={s.cardTitle}>{card.title}</h4>
-      </div>
-      <img className={s.cardPic} src={card.picSrc} alt={card.alt} />
-    </div>
-  );
-}
+// function Card({ card }) {
+//   return (
+//     <div className={`${s.card}`}>
+//       <div className={s.cardTextBlock}>
+//         <h4 className={s.cardTitle}>{card.title}</h4>
+//       </div>
+//       <img className={s.cardPic} src={card.picSrc} alt={card.alt} />
+//     </div>
+//   );
+// }
